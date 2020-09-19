fddf56baf4761bf176c473f90d7856c563d87385
Kuliza-280 <psnitsri01@gmail.com>
2020-09-19 21:49:04 +0530

    Adding button counter using state hooks

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-const App = () => (
-  <div>
-    <h1>Lets Build Stuff</h1>
-  </div>
-);
+const App = () => {
+  const [state, setState] = React.useState(0);
+  return (
+    <div>
+      <h1>Lets Build Stuff</h1>
+      <button onClick={() => setState((state) => state + 1)}>{state}</button>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("root"));
